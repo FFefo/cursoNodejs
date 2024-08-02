dd73fae51a1d4135ecfebdeea55198ddda22feb1
FFefo <felpo.correiacosta@gmail.com>
2024-08-02 10:51:10 -0300

    testes 13 ao 18

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 
 const servidor = express();
+servidor.use(express.json());
 
 servidor.get('/helloworld', (req, resp) => {
 
@@ -12,7 +13,7 @@ servidor.get('/mensagem/boasvindas', (req, resp) => {
 
 })
 
-servidor.get('/v2/mensagem/boasvindas', (req, resp) =>{
+servidor.get('/v2/mensagem/boasvindas', (req, resp) => {
     resp.send('SEU BOSTINHA')
 
 })
@@ -33,6 +34,57 @@ servidor.get('/calculadora/somar/:n1/:n2', (req, resp) => {
     let soma = n1 + n2
     resp.send('A soma é ' + soma)
 
+})
+
+servidor.post('/media', (req, resp) => {
+    //parâmetro de corpo
+    let n1 = req.body.nota1;
+    let n2 = req.body.nota2;
+    let n3 = req.body.nota3;
+
+    let media = (n1+n2+n3) / 3;
+
+    resp.send('A média é ' + media);
+
+})
+
+servidor.post('/loja/pedido', (req, resp) => {
+    let total = req.body.total;
+    let parcelas = req.body.parcelas;
+    let cupom = req.query.cupom;
+
+    if (parcelas > 1) {
+        let juros = total * 0.05;
+        total += juros
+    }
+
+    if (cupom == 'QUERO100'){
+        total -= 100;
+    }
+
+    resp.send('O total do pedido ficou em R$ ' + total)
+})
+
+servidor.post('/loja/pedido/completo', (req, resp) =>{
+    let parcelas = req.body.parcelas;
+    let itens = req.body.itens;
+    let cumpom = req.query.cupom;
+
+    let total = 0
+    for(let produto of itens){
+        total += produto.preco
+    }
+
+    if (parcelas > 1){
+        let juros = total + 0.05;
+        total += juros
+    }
+
+    if (cupom == 'QUERO100'){
+        total -= 100;
+    }
+
+resp.send('O total a pagar é R$ ' + total)
 })
 
 servidor.listen(
